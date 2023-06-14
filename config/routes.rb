5122703613c64b5914524002f7e62cb26a480917
lawrahkonwea <lawrahcollections@gmail.com>
2023-06-14 08:39:18 +0100

    Create and add data to user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'post/index'
+  # get 'post/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
