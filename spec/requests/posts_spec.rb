0acbe16f32cc24dad6010c9e4e19959ec2e63194
lawrahkonwea <lawrahcollections@gmail.com>
2023-06-14 11:25:19 +0100

    Add tests for posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   context 'GET /index' do
+    it 'returns http success' do
+      get '/users/1/posts'
+      expect(response).to be_successful
     end
+
+    it 'renders the correct template' do
+      get '/users/1/posts'
+      expect(response).to render_template('index')
+    end
+  end
 
     
