bedb7970a652ab161a0398867ca079c39071d3ff
lawrahkonwea <lawrahcollections@gmail.com>
2023-06-14 11:26:13 +0100

    Add tests for posts show

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe 'Posts', type: :request do
     end
   end
 
+  context 'GET /show' do
+    it 'returns http success' do
+      get '/users/1/posts/1'
+      expect(response).to be_successful
+    end
+
+    it 'renders the correct template' do
+      get '/users/1/posts/1'
+      expect(response).to render_template('show')
+    end
+  end
+end
+
     
