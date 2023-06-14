79ad3542bbadb349e7718ce50ec1fcd44484cc44
lawrahkonwea <lawrahcollections@gmail.com>
2023-06-14 11:23:07 +0100

    Add test for user

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns http success' do
+      get '/users'
+      expect(response).to be_successful
+    end
+
+    it 'renders the correct template' do
+      get '/users'
+      expect(response).to render_template('index')
+    end
+
+    it 'should include correct placeholder' do
+      get '/users'
+      expect(response.body).to include('')
+    end
   end
+
+ 
 end
