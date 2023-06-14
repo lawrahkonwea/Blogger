8466c27a87e476186a914bf32c083caaeef2ea58
lawrahkonwea <lawrahcollections@gmail.com>
2023-06-14 10:43:14 +0100

    Add rails controller testing to gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index, :show] do
-  resources :posts, only: [:index, :show] 
+   resources :posts, only: [:index, :show] 
  # Defines the root path route ("/")
   end
 
