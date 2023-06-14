df808f9a767f8f40088ba81e0adc00cde5eb184e
lawrahkonwea <lawrahcollections@gmail.com>
2023-06-14 09:02:42 +0100

    Add routes to the index and show pages of the of the user post section

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  # get 'post/index'
-  # get 'post/show'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "users#index"
+  resources :users, only: [:index, :show] do
+  resources :posts, only: [:index, :show] 
+ # Defines the root path route ("/")
+  end
+
+  root "users#index"
 end
