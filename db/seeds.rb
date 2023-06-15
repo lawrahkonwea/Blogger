8b2a8867f289c56a6fed179435f6fca337980296
lawrahkonwea <lawrahcollections@gmail.com>
2023-06-15 09:37:08 +0100

    Add post data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,13 @@
 first_user = User.create(name: 'Amaka', photo: 'https://avatars.githubusercontent.com/u/105006208?v=4', bio: 'Teacher from Mexico.')
 second_user = User.create(name: 'Ifeanyi', photo: 'https://avatars.githubusercontent.com/u/105528809?v=4', bio: 'Teacher from Poland.')
 third_user = User.create(name: 'Mildred', photo: 'https://avatars.githubusercontent.com/u/78565848?v=4', bio: 'Teacher from Hungary.')
+
+first_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'ONE This is my first post')
+second_post = Post.create(author_id: first_user.id, title: 'Hi there', text: 'ONE This is my second post')
+third_post = Post.create(author_id: first_user.id, title: 'Holaaa', text: 'ONE This is my third post')
+fourth_post = Post.create(author_id: second_user.id, title: 'Hello', text: 'TWO This is my first post')
+fifth_post = Post.create(author_id: second_user.id, title: 'Hey hey', text: 'TWO This is my second post')
+sixth_post = Post.create(author_id: second_user.id, title: 'Testing', text: 'TWO This is my third post')
+seventh_post = Post.create(author_id: third_user.id, title: 'Checking', text: 'THREE This is my first post')
+eight_post = Post.create(author_id: third_user.id, title: 'Another day to slay', text: 'THREE This is my second post')
+nineth_post = Post.create(author_id: third_user.id, title: 'favorites', text: 'THREE This is my third post')
