58113d0d6de85cd97fd2e9341f538aedb2a18bc6
lawrahkonwea <lawrahcollections@gmail.com>
2023-06-15 10:46:56 +0100

    Display users details on show and update index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  def index; end
+  def index;
+  
+ end
 
-  def show; end
+  def show;
+  
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
  # Defines the root path route ("/")
   end
 
-  root "users#show"
+  root "users#index"
 end
