d5eac689c0af68ebd2414eaa2785f24ad0fcec08
lawrahkonwea <lawrahcollections@gmail.com>
2023-06-15 09:30:36 +0100

    Add user data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_user = User.create(name: 'Amaka', photo: 'https://avatars.githubusercontent.com/u/105006208?v=4', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Ifeanyi', photo: 'https://avatars.githubusercontent.com/u/105528809?v=4', bio: 'Teacher from Poland.')
+third_user = User.create(name: 'Mildred', photo: 'https://avatars.githubusercontent.com/u/78565848?v=4', bio: 'Teacher from Hungary.')
