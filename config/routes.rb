414643f7fdec4520d1dc618443f4fea2a858a5ea
lawrahkonwea <lawrahcollections@gmail.com>
2023-06-16 03:29:49 +0100

    Updates routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index, :show] do
-   resources :posts, only: [:index, :show] 
- # Defines the root path route ("/")
+  resources :posts, only: [:index, :show, :new, :create] do
+  resources :comments, only: [:new, :create]
+  resources :likes, only: [:create]
+  end
   end
 
   root "users#index"
